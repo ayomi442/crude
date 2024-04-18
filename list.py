e9df81f907054046acf3b36ef81dd6ed08049749
ayomi442 <167423652+ayomi442@users.noreply.github.com>
2024-04-18 07:12:27 -0400

    Add files via upload

---
+++ list.py
@@ -0,0 +1,45 @@
+# cars = ["volvo", "benz","lexus", "toyota"]
+# for i in range(len(cars)):
+#     if cars[i] =="toyota":
+#         cars.remove("toyota")
+#     for car in cars:
+
+
+# names = []
+# for i in range(5):
+#     name = input("enter your student name: ")
+#     names.append(name)
+# print(names)
+
+# sum = 0
+# num = [20,90,70]
+# for x in range(len(num)):
+#      sum = sum + num[x]
+#      print(sum)
+
+# scores = []
+# total = 0
+# for i in range(5):
+#     score = int(input("enter your score"))
+#     scores.append(score)
+#     for x in range(len(scores)):
+#         total = total + scores[x]
+#         print(f"total score is {total}")
+
+# scores = []
+# for i in range(5):
+#     score = int(input("Enter your scores:"))
+#     scores.append(score)
+#     total = 0
+#     for x in range(len(scores)):
+#         total = total + scores[x]
+# print(f" Your total score is {total}")
+
+
+
+
+
+
+
+
+
